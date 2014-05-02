0b66535101b45289a2e8c459c3467a787e613bda
Sam <manuakasam@googlemail.com>
2014-05-02 13:16:40 +0200

    Refactor the domain from "Album" to "Blog"

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+// Filename: /module/Blog/Module.php
+namespace Blog;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface
+{
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    // Autoload all classes from namespace 'Blog' from '/module/Blog/src/Blog'
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                )
+            )
+        );
+    }
+
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+} 
